d9bf0d204467e486a780718ada6c10c74fbf45f5
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-07-10 19:57:43 +0200

    Get/Set methods exercise...

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ class Car {
   constructor(make, model, weight, color) {
     this.make = make;
     this.model = model;
-    this.weight = weight;
+    this._weight = weight;
     this.color = color;
   }
 
-  getName() {
+  get name() {
     return this.make + " " + this.model;
+  }
+
+  set weight(weight) {
+    if (isNaN(this.weight)) {
+      this._weight = weight;
+    } else {
+      throw new TypeError("Value of weight must be a number!");
+    }
+  }
+
+  get weight() {
+    return this._weight + "kg";
   }
 
   getInfo() {
@@ -27,10 +39,16 @@ class Car {
       this.color
     );
   }
+
+  static kwToHp(kw) {
+    return (kw * 1.34102).toFixed(2);
+  }
 }
 
 var car1 = new Car("Subary", "Legacy", 1563, "black");
 var car1 = new Car("Ford", "Taurus", 1876, "blue");
 var car1 = new Car("Porsche", "Panamera", 1563, "grey");
 
-console.log(car1.getName());
+car1.weight = 1900;
+
+console.log(Car.kwToHp(250));
